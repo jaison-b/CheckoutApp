3b6f4943a9d8b972cdbee1d8f9e775da5dfab0fe
Jaison Baskaran <jbaskaran@streamsystems.ca>
2017-01-31 10:37:15 -0700

    intial checkin

--- CheckoutApp/Program.cs
+++ CheckoutApp/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Fclp;
 
 namespace CheckoutApp
 {
@@ -10,6 +7,46 @@ namespace CheckoutApp
     {
         static void Main(string[] args)
         {
+            var parser = getParser();
+            var parseResult = parser.Parse(args);
+            if (parseResult.HasErrors)
+            {
+                parser.HelpOption.ShowHelp(parser.Options);
+                Environment.Exit(-1);
+            }
+
+            Console.WriteLine("Executing: {0}", parser.Object.InputFile);
+        }
+
+        private static FluentCommandLineParser<ApplicationArguments> getParser()
+        {
+            FluentCommandLineParser<ApplicationArguments> parser = new FluentCommandLineParser<ApplicationArguments>();
+            parser.Setup(arg => arg.InputFile)
+                .As('i', "input-file")
+                .Required()
+                .WithDescription("Order file to process");
+            parser.Setup(arg => arg.ProductsFile)
+                .As('p', "products-file")
+                .Required()
+                .WithDescription("Products file to lookup");
+            parser.Setup(arg => arg.PromotionsFile)
+                .As('d', "discounts-file")
+                .Required()
+                .WithDescription("Discounts file to apply to products");
+            parser.SetupHelp("?", "help")
+                .Callback(text => Console.WriteLine(text))
+                .WithHeader("Usage: CheckoutApp -i ordersfile.txt -p prodsfile.txt -d promosfile.txt\n" +
+                            "       CheckoutApp --input-File ordersfile.txt --products-file prodsfile.txt --discounts-file promosfile.txt");
+
+            return parser;
         }
     }
+
+    public class ApplicationArguments
+    {
+        public string InputFile { get; set; }
+        public string ProductsFile { get; set; }
+        public string PromotionsFile { get; set; }
+    }
+
 }
